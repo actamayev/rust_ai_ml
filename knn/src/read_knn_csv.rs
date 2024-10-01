7010403ad6a8c9d1fd48754e7928f768bf9a61ec
atamayev <75034340+atamayev@users.noreply.github.com>
2024-10-01 18:19:02 +0300

    Clean csv reader

--- knn/src/read_knn_csv.rs
+++ knn/src/read_knn_csv.rs
@@ -4,30 +4,16 @@ use csv::ReaderBuilder;
 
 pub fn read_knn_csv(file_path: &str) -> Result<Vec<Vec<f32>>, Box<dyn Error>> {
     let file = File::open(file_path)?;
-
     let mut rdr = ReaderBuilder::new().from_reader(file);
 
-    let mut data_to_return = Vec::new();
+    let data_to_return: Result<Vec<Vec<f32>>, Box<dyn Error>> = rdr.records()
+        .map(|result| {
+            let record = result?;
+            record.iter()
+                .map(|field| field.parse::<f32>().map_err(|e| Box::new(e) as Box<dyn Error>)) // Convert ParseFloatError to Box<dyn Error>
+                .collect::<Result<Vec<f32>, _>>() // Collect the parsed fields
+        })
+        .collect(); // Collect all rows
 
-    // Read the records from CSV
-    for result in rdr.records() {
-        let record = result?;
-
-        let sq_footage = record[0].parse()?;
-        let num_bedrooms = record[1].parse()?;
-        let house_age = record[2].parse()?;
-        let location_rating = record[3].parse()?;
-        let house_price = record[4].parse()?;
-
-        // Store the features and target values
-        data_to_return.push(vec![
-            sq_footage,
-            num_bedrooms,
-            house_age,
-            location_rating,
-            house_price
-        ]);
-    }
-
-    Ok (data_to_return)
+    data_to_return
 }
